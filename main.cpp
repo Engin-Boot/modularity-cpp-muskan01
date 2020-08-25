80b793862570385e40d3920d17708ebd0ed232b8
muskan01 <35635037+muskan01@users.noreply.github.com>
2020-08-25 12:23:45 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -26,6 +26,13 @@ void testPairToNumber(
     assert(pairNumber == expectedPairNumber);
 }
 
+void ToString()
+{
+    for(int pair_numb= 1; pair_numb < 26; pair_numb++){
+        TelCoColorCoder::ColorPair pair = TelCoColorCoder::GetColorFromPairNumber(pair_numb);
+        std::cout<<"Pair number: "<<pair_numb<<" "<<"Color Pair: "<<pair.ToString()<<std::endl;
+    }
+
 int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
@@ -33,5 +40,6 @@ int main() {
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
+    ToString();
     return 0;
 }
