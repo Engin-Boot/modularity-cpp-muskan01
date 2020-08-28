32d057c773f621496d0050b845540c92675ee4d5
muskan01 <35635037+muskan01@users.noreply.github.com>
2020-08-28 20:16:22 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,23 @@
 using namespace TelCoColorCoder;
 
 void ToString()
-{   
-    for(int pair_numb= 1; pair_numb < 26; pair_numb++){
-        TelCoColorCoder::ColorPair pair = TelCoColorCoder::GetColorFromPairNumber(pair_numb);
-        std::cout<<"Pair number: "<<pair_numb<<" "<<"Color Pair: "<<pair.ToString()<<std::endl;
+{   int PairNumber = 0;
+    String Manual = " ";
+    String TempManual = " ";
+   String PairNumberInString = " ";
+ 
+    for(int MajorColorno= 0; MajorColorno < TelCoColorCoder:: numberOfMajorColors ; MajorColorno++){
+        for(int MinorColorno= 0; MinorColorno < TelCoColorCoder:: numberOfMinorColors ; MinorColorno++)
+ {           TempManual = " ";
+       PairNumber = PairNumber + 1;
+      TelCoColorCoder::ColorPair pair = TelCoColorCoder::GetColorFromPairNumber(PairNumber);
+        TempManual = pair.ToString();
+      PairNumberInString = std::to_string(PairNumber);
+    } 
+        Manual = PairNumberInString + ". " +  PairNumberInString + "\n";
     }
-}
+ cout << Manual << endl;
+} 
 
 int main() {
     TelCoColorCoder::testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
